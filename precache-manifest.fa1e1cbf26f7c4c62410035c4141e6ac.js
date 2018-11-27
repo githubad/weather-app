eb809e9c77aa7ae50ce86e6c4fe25a99c0543810
Adnan Qaizar <adnanqaizar@gmail.com>
2018-11-28 01:20:18 +0500

    Updates

---
+++ precache-manifest.fa1e1cbf26f7c4c62410035c4141e6ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "035aadec4d6f37ad75b6",
+    "url": "/weather-app/static/js/main.035aadec.chunk.js"
+  },
+  {
+    "revision": "c6a90e539878bb1c9d83",
+    "url": "/weather-app/static/js/1.c6a90e53.chunk.js"
+  },
+  {
+    "revision": "035aadec4d6f37ad75b6",
+    "url": "/weather-app/static/css/main.62ce72ff.chunk.css"
+  },
+  {
+    "revision": "79ff008b2480ff42ab7b2029806ad238",
+    "url": "/weather-app/index.html"
+  }
+];
